05b25eed2be60c90dfb003a8b231888375c02a7e
Liam Söderholm <liam.soderholm19@gmail.com>
2025-10-01 09:08:19 +0200

    Klar

---
+++ app.ts
@@ -0,0 +1,201 @@
+/*let age = +await read("Vad är din ålder:  ")
+
+if (age >=  18) {
+    write("Du är myndig")
+} else if (age <= 1 || age <= 18) {
+    write("Du är inte myndig") 
+}*/
+/*let age = +await read("Hur lång är du:  ")
+
+if (age >=  200) {
+    write("Hej långe du!")
+} else if (age <= 1 || age <= 200) {
+    write("Hej korte du!") 
+}*/
+/*let age = +await read("Ditt tal:  ")
+
+if (age >=  1) {
+    write("Positivt tal")
+} else if (age <= -1) {
+    write("Negativt tal") 
+} else if (age => 0) {
+    write("Talet är noll") 
+}*/
+/*let X = +await read("X:  ")
+let Y = +await read("Y:  ")
+
+if (X > Y) {
+    write("X större än Y")
+} else if (Y > X) {
+    write("Y större än X") 
+} else if (X = Y) {
+    write("X samma som Y") 
+}*/
+/*let X = +await read("X:  ")
+let Y = +await read("Y:  ")
+let Operator = await read("Operator")
+
+if (Operator=="+") {
+    let Summa = X+Y
+    write("Summa "+Summa)
+} else if (Operator=="-") {
+    let Summa = X-Y
+    write("Summa "+Summa)
+} else if (Operator=="/") {
+    let Summa = X/Y
+    write("Summa "+Summa)
+} else if (Operator=="*") {
+    let Summa = X*Y
+    write("Summa "+Summa)
+}*/
+/*let Kort = +await read("Kort:  ")
+
+if (Kort==1) {
+    write("Ess")
+} else if (Kort==11) {
+    write("Knekt ")
+} else if (Kort==12) {
+    write("Dam ")
+    } else if (Kort==13) {
+    write("Kung ")
+} else   {
+    write("En "+Kort+":a")
+}*/
+/*let Månad = +await read("Månadsnummer:  ")
+
+if (Månad==1) {
+    write("Januari")
+} else if (Månad==2) {
+    write("Februari ")
+} else if (Månad==3) {
+    write("Mars ")
+    } else if (Månad==4) {
+    write("April ")
+ } else if (Månad==5) {
+    write("Maj ")
+     } else if (Månad==6) {
+    write("Juni ")
+     } else if (Månad==7) {
+    write("Juli ")
+     } else if (Månad==8) {
+    write("Augusti ")
+     } else if (Månad==9) {
+    write("September ")
+     } else if (Månad==10) {
+    write("Oktober ")
+     } else if (Månad==11) {
+    write("November ")
+     } else if (Månad==12) {
+    write("December ")}*/
+  /*let Månad = +await read("Månadsnummer:  ")
+
+if (Månad==1) {
+    write("Vinter")
+} else if (Månad==2) {
+    write("Vinter ")
+} else if (Månad==3) {
+    write("Vår ")
+    } else if (Månad==4) {
+    write("Vår ")
+ } else if (Månad==5) {
+    write("Vår ")
+     } else if (Månad==6) {
+    write("Sommar ")
+     } else if (Månad==7) {
+    write("Sommar ")
+     } else if (Månad==8) {
+    write("Sommar ")
+     } else if (Månad==9) {
+    write("Höst ")
+     } else if (Månad==10) {
+    write("Höst ")
+     } else if (Månad==11) {
+    write("Höst ")
+     } else if (Månad==12) {
+    write("Vinter ")}*/
+/*let X = +await read("Tal 1:  ")
+let Y = +await read("Tal 2:  ")
+let Z = +await read("Gissning:  ")
+let Produkt=X*Y
+
+if (Z==Produkt){
+    write ("Rätt svar")
+}
+else {
+    write ("Fel. Rätt svar: "+Produkt)
+}*/
+/*let X = +await read("Tal 1:  ")
+let Y = +await read("Tal 2:  ")
+let Summa=X+Y
+let Medel=(X+Y) /2 
+
+write ("Summa:  "+Summa)
+write ("Medel:  "+Medel)
+if (X>Y){
+    write ("Mindre:  "+Y)
+    write ("Största: "+X)
+}
+else if (X<Y){
+    write ("Mindre:  "+X)
+    write ("Största: "+Y)
+}
+else {
+    write ("Lika stora:  "+X)
+}*/
+/*let X = +await read("Tal 1:  ")
+let Y = +await read("Tal 2:  ")
+let Z = +await read("Tal 3:  ")
+let Summa=X+Y+Z
+let Medel=(X+Y+Z) /3 
+
+write ("Summa:  "+Summa)
+write ("Medel:  "+Medel)
+let minsta = Math.min(X, Y, Z) 
+let storsta = Math.max(X, Y, Z)
+let mellan = X + Y + Z - minsta - storsta
+if (X==Y && Y==Z){
+    write ("Lika stora:  "+X)
+}
+else {
+    write ("Minsta:  "+minsta)
+    write ("Mellan:  "+mellan)
+    write ("Största: "+storsta)
+}*/
+/*let A = await read("Bokstav:  ")
+if (A=="a" || A=="b" || A=="c" || A=="d" || A=="e" || A=="f" || A=="g" || A=="h" || A=="i" || A=="j" || A=="k" || A=="l" || A=="m" || A=="n" || A=="o" || A=="p" || A=="q" || A=="r" || A=="s" || A=="t" || A=="u" || A=="v" || A=="w" || A=="x" || A=="y" || A=="z"){
+    write("Liten bokstav")
+}
+else if (A=="A" || A=="B" || A=="C" || A=="D" || A=="E" || A=="F" || A=="G" || A=="H" || A=="I" || A=="J" || A=="K" || A=="L" || A=="M" || A=="N" || A=="O" || A=="P" || A=="Q" || A=="R" || A=="S" || A=="T" || A=="U" || A=="V" || A=="W" || A=="X" || A=="Y" || A=="Z"){
+    write("Stor bokstav")
+}*/
+/*let A = +await read("Tal:  ")
+if (-10<A && A<10){
+    write ("En siffra")
+}
+else if (-100<A && A<100){
+    write ("Två siffror")
+}
+else if (-1000<A && A<1000){
+    write ("Tre siffror")
+}
+else if (A===1000 || A===-1000){
+    write ("Fyra siffror")
+}
+if (A>0){
+    write ("Positivt tal")
+}
+else if (A<0){
+    write ("Negativt tal")
+}
+else{
+    write ("Talet är noll")
+}*/
+/*let A = +await read("Sida 1:  ")
+let B = +await read("Sida 2:  ")
+let C = +await read("Sida 3:  ")
+if (A+B>C && A+C>B && B+C>A){
+    write ("Det går att skapa en triangel")
+}
+else {
+    write ("Nej! Det går inte att skapa en triangel")
+}*/
